77e661ac9fdceb2bdf7640f432db0ce139609ea8
jvpchaves <117786429+jvpchaves@users.noreply.github.com>
2024-09-29 01:47:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ function calcularIMC() {
     var altura = parseFloat(document.getElementById('altura').value);
     var resultadoDiv = document.getElementById('resultado');
     
-    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0)
+    if  (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0)
          {
         alert('Por favor, insira valores válidos.');
         return;
-        
-        } else {
-
-      
+         
+         } else {  
 
     var imc = peso / (altura * altura);
     var classificacao = '';
